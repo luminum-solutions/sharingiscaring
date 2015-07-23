d02f9e4f4eefc2704924760e309b0f7f2ce506f7
Zowie <zowielangdon@gmail.com>
2015-07-23 20:57:58 +0200

    Removed obsolte Facebook example factory

--- src/sharingiscaring.js
+++ src/sharingiscaring.js
@@ -240,23 +240,7 @@ angular.module('Akoten.sharingiscaring', [])
             },
             transclude: true
         }
-    }]).factory('sicFacebookService', function ($q) {
-        return {
-            getMyLastName: function () {
-                var deferred = $q.defer();
-                FB.api('/me', {
-                    fields: 'last_name'
-                }, function (response) {
-                    if (!response || response.error) {
-                        deferred.reject('Error occured');
-                    } else {
-                        deferred.resolve(response);
-                    }
-                });
-                return deferred.promise;
-            }
-        }
-    }).directive("sicFacebookLogin", [function () {
+    }]).directive("sicFacebookLogin", [function () {
         return {
             controller: function ($scope) {
 
